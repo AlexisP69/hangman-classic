6f5dde79baeb8022522c8ccda8e645aafe791148
APOINSIGNON <alexis.poinsignon@ynov.com>
2021-10-28 12:35:15 +0200

    You won message

--- test.go
+++ test.go
@@ -14,61 +14,97 @@ func main() {
 }
 
 type HangManData struct {
-	Word             string     // Word composed of '_', ex: Hll
+	Word             string     // Word composed of '_', ex: H_ll_
 	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
 	Attempts         int        // Number of attempts left
 	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
 	Correct          string
 }
 
-var s, entree string
 var tab []string
 var col []rune
+var tab2 []string
 
 func Read() {
 
-	file, err := os.Open("words.txt")
+	isdommage := true
 
-	if err != nil {
+	file, err := os.Open("words.txt")
+	// file2, erreur := os.Open("hangman.txt")
+
+	if err != nil { //fichier mot
 		log.Fatal(err)
 	}
 
-	defer file.Close()
+	// if erreur != nil { //fichier position (hangman)
+	// 	log.Fatal(erreur)
+	// }
 
-	scanner := bufio.NewScanner(file)
+	defer file.Close() //fermer le fichier mot
+
+	// defer file2.Close() //fermer le fichier position (hangman)
+
+	scanner := bufio.NewScanner(file) //Lis mon fichier words
 	for scanner.Scan() {
 		tab = append(tab, scanner.Text())
 		if err := scanner.Err(); err != nil {
 			log.Fatalln(err)
 		}
 	}
+
+	// position := bufio.NewScanner(file2) //Lis mon fichier position (hangman)
+	// for position.Scan() {
+	// 	tab2 = append(tab2, position.Text())
+	// 	if erreur := position.Err(); erreur != nil {
+	// 		log.Fatalln(erreur)
+	// 	}
+	// }
+
 	rand.Seed(time.Now().UnixNano())
 	min := 0
 	max := len(tab)
 	i := rand.Intn((max - min))
 
-	mot := tab[i]
+	mot := tab[i] // Mot aléatoire du fichier words
 	fmt.Println(mot)
-	tabmot := []rune(mot)
-	fmt.Println(tabmot)
+	tabmot := []rune(mot) // fmt.Println(tabmot) Affiche les rune des underscore
 	for u := 0; u < len(tabmot); u++ {
-		fmt.Printf("")
+		fmt.Printf("_")
 		col = append(col, '_')
-
 	}
+
+	fmt.Print("\n")
+	fmt.Print("Choissisez votre lettre: ")
+
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		if err != nil {
 			println(err)
 		}
 		input, _ := reader.ReadString('\n')
-		// fmt.Print(input)
-		tabrun := []rune(input)
+		tabrun := []rune(input) //la rune de notre input
+
+		isdommage = true
+
 		for x := 0; x < len(tabmot); x++ {
-			if tabmot[x] == tabrun[0] {
+			if tabmot[x] == tabrun[0] { // compare l'index du mot a l'index de mon input
 				col[x] = tabmot[x]
-				fmt.Println(string(col))
+				isdommage = false
 			}
+		}
+		fmt.Println(string(col)) //Affiche le mot avec les underscore modifier
+		fmt.Print("Choissisez votre lettre: ")
+
+		if isdommage == true {
+			fmt.Println("DOMMAGE CETTE LETTRE N'EST PAS DANS CE MOTS")
+			fmt.Print("\n")
+			// fmt.Println(tab2)
+			fmt.Println(string(col))
+			fmt.Print("Choissisez votre lettre: ")
+		}
+		if mot == string(col) {
+			fmt.Print("\n")
+			fmt.Println("Congratulation You found the word")
 		}
 	}
 }
